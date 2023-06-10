11286d3f059bd5557145db0f58955e0bb0b839d5
youzhou19858 <yzhou750@student.sfbu.edu>
2023-06-09 22:51:18 -0700

    convert identation to spaces

--- CE450/Lab2/question1.c
+++ CE450/Lab2/question1.c
@@ -2,41 +2,41 @@
 #include <stdio.h>
 
 void turn_on(int channel){
-	digitalWrite(channel,LOW);
+  digitalWrite(channel,LOW);
 }
 
 void turn_off(int channel){
-	digitalWrite(channel,HIGH);
+  digitalWrite(channel,HIGH);
 }
 
 void setup(){
-	int i;
-	for(i=0;i<8;i++){
-		pinMode(i,OUTPUT);
-		digitalWrite(i,HIGH);
-	}
+  int i;
+  for(i=0;i<8;i++){
+    pinMode(i,OUTPUT);
+    digitalWrite(i,HIGH);
+  }
 }
 
 int main(){
-	int i;
-	if(wiringPiSetup()==-1){
-		printf("setup wiringPi failed!\n");
-		printf("please check your setup\n");
-		return -1;
-	}
+  int i;
+  if(wiringPiSetup()==-1){
+    printf("setup wiringPi failed!\n");
+    printf("please check your setup\n");
+    return -1;
+  }
 
-	setup();
-	while(1){
-		for(i=0;i<8;i++){
-			turn_on(i);
-			delay(512);
-			turn_off(i);
-		} 
-		for(i=7;i>=0;i--){
-			turn_on(i);
-			delay(512);
-			turn_off(i);
-		}
-	}
-	return 0;
+  setup();
+  while(1){
+    for(i=0;i<8;i++){
+      turn_on(i);
+      delay(512);
+      turn_off(i);
+    } 
+    for(i=7;i>=0;i--){
+      turn_on(i);
+      delay(512);
+      turn_off(i);
+    }
+  }
+  return 0;
 }
