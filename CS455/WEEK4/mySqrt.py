e98ef16e6f8aff112505b5c36cce64ae90aeb653
youzhou19858 <yzhou750@student.sfbu.edu>
2023-06-16 19:24:02 -0700

    week4

---
+++ CS455/WEEK4/mySqrt.py
@@ -0,0 +1,18 @@
+def mySqrt(x):
+    if x < 2:
+        return x
+    left, right = 2, x // 2
+    while left <= right:
+        mid = left + (right - left) // 2
+        num = mid * mid
+        if num > x:
+            right = mid - 1
+        elif num < x:
+            left = mid + 1
+        else:
+            return mid
+    return right
+
+# Test the function
+x = 8
+print(mySqrt(x))  # Output: 2
