352827c82641c72111cfa3aaf5e4cd6f9b94fbb7
youzhou19858 <yzhou750@student.sfbu.edu>
2023-06-24 02:09:33 -0700

    final

--- CE450/Lab4/lab4.c
+++ CE450/Lab4/lab4.c
@@ -1,40 +1,24 @@
 #include <stdio.h>
 #include <wiringPi.h>
 
-#define BeepPin 0
-
-void turn_on(int channel) { digitalWrite(channel, LOW); }
-
-void turn_off(int channel) { digitalWrite(channel, HIGH); }
+#define BUZZER 17
+#define LED 18
 
 int main(void) {
-  if (wiringPiSetup() == -1) {
-    printf("setup wiringPi failed !");
-    return 1;
-  }
-
-  void setup() {
-    for (int i = 0; i < 8; i++) {
-      pinMode(i, OUTPUT);
-      digitalWrite(i, HIGH);
-    }
-  }
+  pinMode(BUZZER, OUTPUT);
+  digitalWrite(BUZZER, HIGH);
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, HIGH);
 
   while (1) {
-    // beep on
     printf("Buzzer on\n");
-    digitalWrite(BeepPin, LOW);
-    for (int i = 1; i < 8; ++i) {
-      turn_on(7);
-    }
-    delay(100);
-    for (int i = 1; i < 8; ++i) {
-      turn_off(7);
-    }
+    digitalWrite(BUZZER, LOW);
+    digitalWrite(LED, LOW);
+    delay(256);
+    digitalWrite(LED, HIGH);
+    digitalWrite(BUZZER, HIGH);
     printf("Buzzer off\n");
-    // beep off
-    digitalWrite(BeepPin, HIGH);
-    delay(100);
+    delay(128);
   }
 
   return 0;
