8146a8528c71d772944742e8d014ed161b4bff64
yzhou750 <yzhou750@student.sfbu.edu>
2023-07-30 21:13:16 +0100

    LCD1602 FROM SUNFOUNDER

---
+++ CE450/Lab9/lab9.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+from signal import signal, SIGTERM, SIGHUP, pause
+from rpi_lcd import LCD
+from sys import version_info
+import time
+
+if version_info.major == 3:
+    raw_input = input
+def print_msg():
+    print ("========================================")
+    print ("|                LCD1602               |")
+    print ("|    ------------------------------    |")
+    print ("|         GND connect to PIN 6         |")
+    print ("|         VCC connect to PIN 2         |")
+    print ("|         SDA connect to PIN 3         |")
+    print ("|         SCL connect to PIN 5         |")
+    print ("|                                      |")
+    print ("|           Control LCD1602            |")
+    print ("|                                      |")
+    print ("|                              You Zhou|")
+    print ("========================================\n")
+    print ("Program is running...")
+    print ("Please press Ctrl+C to end the program...")
+    raw_input ("Press Enter to begin\n")
+
+lcd = LCD()
+def safe_exit(signum, frame):
+    exit(1)
+
+signal(SIGTERM, safe_exit)
+signal(SIGHUP, safe_exit)
+
+def main():
+    print_msg()
+    txt = "you did good job"
+    a = 0
+    while True:
+        if a + 16 < len(txt):
+            lcd.text(txt[a:a+16],1)
+        if a + 16 >= len(txt):
+            b = txt[a:len(txt)]
+            c = 16 - len(b)
+            lcd.text(b + '   ' + txt[0:c], 1)
+        if a == len(txt):
+            a = -1
+        a += 1
+        time.sleep(0.5)
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        lcd.clear()
